e9793f2c6050fce77c0cf7666953de3c35837e4e
aleffalves <aleff.alves44@gmail.com>
2022-01-20 23:40:23 -0300

    add projeto

---
+++ src/main/java/com/github/aleffalves/apibarber/ApiBarberApplication.java
@@ -0,0 +1,13 @@
+package com.github.aleffalves.apibarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBarberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aleffalves/apibarber/ApiBarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aleffalves.apibarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
