aa821ca7f855887864e434ce20ccb07937cf4743
aleffalves <aleff.alves44@gmail.com>
2022-02-28 21:07:38 -0300

    deploy

--- src/main/java/com/github/aleffalves/apibarber/security/WebSecurityConfig.java
+++ src/main/java/com/github/aleffalves/apibarber/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.github.aleffalves.apibarber.security;
 
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
